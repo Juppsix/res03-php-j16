8cd1eb39a9ee16d12b552b406dd146cf502b8acd
Juppsix <julien.postecc@gmail.com>
2023-02-17 14:59:56 +0100

    oui

--- wp-content/themes/yep/front-page.php
+++ wp-content/themes/yep/front-page.php
@@ -26,14 +26,19 @@
 </nav>
 </header>
 <main>
-<section>
+<section class="section1">
 <h1>YEPFOLIO<h1>
 <h2>DEVELOPPEMENT WEB & MOBILE</h2>
 </section>
 
-<section>
+<section class="section2">
 <h3>YEPFOLIO EN QUELQUES MOTS</h3>
-<p>lorem ipsum..............................<p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+     sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
 </section>
 
 <section>
